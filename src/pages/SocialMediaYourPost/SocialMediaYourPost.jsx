9d9396fea0e90bafc62a707cf50811f782f0046e
sayma078 <somayasayma2@gmail.com>
2022-12-01 23:02:01 +0600

    social media part

--- src/pages/SocialMediaYourPost/SocialMediaYourPost.jsx
+++ src/pages/SocialMediaYourPost/SocialMediaYourPost.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import YourPost from '../../components/SocialPageComponent/YourPost/YourPost/YourPost';
-
+import {useParams} from "react-router-dom";
+import socialActiveUser from "../../services/data/SocialActiveUser"
+import SocialPageHeader from '../../components/SocialPageComponent/SocialPageHeader/SocialPageHeader';
 const SocialMediaYourPost = () => {
+    const {id}=useParams()
+    const findUser = socialActiveUser.find(user => user.id==id)
+    console.log(findUser)
     return (
         <div >
-            <YourPost />
+            <SocialPageHeader />
+            <YourPost findUser={findUser} />
         </div>
     );
 };
---
+++ src/pages/SocialMediaYourProfile/SocialMediaYourProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SocialPageHeader from '../../components/SocialPageComponent/SocialPageHeader/SocialPageHeader';
+import YourProfile from '../../components/SocialPageComponent/YourProfile/YourProfile';
+
+const SocialMediaYourProfile = () => {
+    return (
+        <div>
+            < SocialPageHeader />
+            <YourProfile/>
+        </div>
+    );
+};
+
+export default SocialMediaYourProfile;
